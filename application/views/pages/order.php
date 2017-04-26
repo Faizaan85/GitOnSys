07231c9470502393c5a406175ae6fd5f230886c3
Faizaan85 <faizaan.varteji@gmail.com>
2017-04-26 19:20:12 +0400

    Think the Editing of Qty works for now...

--- application/views/pages/order.php
+++ application/views/pages/order.php
@@ -68,18 +68,18 @@ I realise the name is crappy but not gonna change as it is gonna be difficult. -
                     <?php echo ($i); ?>
                     <span class="pull-right"><span class="fa fa-check fa-lg hidden-print"></span></span>
                 </td>
-                <td class="edit" data-edit-state="false"><?php echo $item['OiPartNo']; ?> <span class="pull-right"><span class="fa fa-pencil-square-o fa-lg hidden-print"></span></span></td>
+                <td class="edit" data-editstate="false"><?php echo $item['OiPartNo']; ?> <span class="pull-right"><span class="fa fa-pencil-square-o fa-lg hidden-print"></span></span></td>
                 <td class="delete"><?php echo $item['OiSupplierNo']; ?><span class="pull-right"><span class="fa fa-times fa-lg hidden-print" aria-hidden="true"></span></span></td>
                 <td><?php echo $item['OiDescription']; ?></td>
 
-                <td class="RQty" type='number'>
-					<input type="text" id="txtRqty" class="edit-toggle" value="<?php echo $item['OiRightQty']; ?>" disabled="true" style="width: 50px;">
+                <td class="RQty">
+					<input type="number" id="txtRqty" class="edit-toggle qty" value="<?php echo $item['OiRightQty']; ?>" min="0" disabled="true" style="width: 50px;">
 				</td>
                 <td class="LQty">
-					<input type="text" id="txtLqty" class="edit-toggle" value="<?php echo $item['OiLeftQty']; ?>" disabled="true" style="width: 50px;">
+					<input type="number" id="txtLqty" class="edit-toggle qty" value="<?php echo $item['OiLeftQty']; ?>" min="0" disabled="true" style="width: 50px;">
 				</td>
                 <td class="TQty">
-					<input type="text" id="txtTqty" class="edit-toggle" value="<?php echo $item['OiTotalQty']; ?>" disabled="true" style="width: 60px;">
+					<input type="number" id="txtTqty" class="edit-toggle" value="<?php echo $item['OiTotalQty']; ?>" min="0" disabled="true" style="width: 60px;">
 				</td>
             </tr>
         <?php $i++;?>
